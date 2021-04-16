fcd89e4a41daf11c4fee9441a4068cb1b0e84443
EC2 Default User <ec2-user@ip-172-16-77-120.ec2.internal>
2021-04-16 16:45:29 +0000

    your message

--- constants.py
+++ constants.py
@@ -3,8 +3,22 @@ EMBEDDING_SIZE = 100
 MAX_LENGTH = 2500
 
 #where you want to save any models you may train
+
 MODEL_DIR = '/path/to/repo/saved_models/'
 
-DATA_DIR = '/path/to/repo/mimicdata/'
-MIMIC_3_DIR = '/path/to/repo/mimicdata/mimic3'
-MIMIC_2_DIR = '/path/to/repo/mimicdata/mimic2'
+DATA_DIR = '../mimicdata/mimic3/results'
+MIMIC_3_DIR = '../mimicdata/mimic3/'
+MIMIC_2_DIR = '../mimicdata/mimic3/'
+
+# MODEL_DIR = 's3://mimic-3-hou/result'
+
+# DATA_DIR = 's3://mimic-3-hou'
+# MIMIC_3_DIR = 's3://mimic-3-hou'
+# MIMIC_2_DIR = 's3://mimic-3-hou'
+
+
+# MODEL_DIR = '/path/to/repo/saved_models/'
+
+# DATA_DIR = '/path/to/repo/mimicdata/'
+# MIMIC_3_DIR = '/path/to/repo/mimicdata/mimic3'
+# MIMIC_2_DIR = '/path/to/repo/mimicdata/mimic2'
